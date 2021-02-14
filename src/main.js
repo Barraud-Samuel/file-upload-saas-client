76b0a0104445b162f4335379fbdf95d5f96bd599
barraud-samuel <samuel.barraud@hetic.net>
2021-02-14 17:32:31 +0100

    ready for prod

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.baseURL = process.env.BASE_URL_API || 'http://localhost:8000'
 axios.defaults.withCredentials = true
 
 store.dispatch('auth/me').then(()=>{
